3dccace122a81c076d623dd734d9d173ae937799
atgao <alice.gao@princeton.edu>
2020-10-23 13:24:19 -0400

    works for n=3

--- RDM.py
+++ RDM.py
@@ -20,19 +20,18 @@ def convert_binary_to_graph(bits, n):
 			row += 1
 			col = row + 1 
 			count -= 1
-	# print(G.astype('uint8'))
-
 	return G
 
 def calculate_prob(bits, G, n, ht={}):
 	'''
 	params: G: graph
-	        n: number of nodes
+			n: number of nodes
 	'''
 
 	# need to calculate probability for each node 
 	m = int(n*(n-1)/2) # total number of matches
-	
+
+	# print("Calculating prob for %s with n = %d" % (bits, n))
 	if n == 2:
 		if int(bits, 2) == 0:
 			return np.array([0, 1])
@@ -40,40 +39,45 @@ def calculate_prob(bits, G, n, ht={}):
 			return np.array([1, 0])
 
 	# get probability each node is not eliminated this round
-	prob = [0.0] * n
+	prob = np.zeros((n), dtype=np.float32)
 
-	# print(G.astype('uint8'))
 	row, col, count = 0, 1, n-1
 	inds = np.arange(n)
 	for i in range(m):
-		# TODO: get actual u, v, wrong answer rn
 		u, v = row, col 
 		res = bits[i]
 
-		print(u, v, "res: %s" % res)
+		# print(u, v, "res: %s" % res)
 
-		# if res == 0:
-			# new_bits = bits[]
-		if res:
-			temp_G = np.delete(G, u, 0)
-			new_G = np.delete(temp_G, u, 1)
-			print(new_G)
+		if res == "0":
+			elim = u
+			# temp_G = np.delete(G, u, 0)
+			# new_G = np.delete(temp_G, u, 1)
+		else:
+			elim = v
+			# temp_G = np.delete(G, v, 0)
+			# new_G = np.delete(temp_G, v, 1)'
+		
+		# eliminate the node 
+		temp_G = np.delete(G, elim, 0)
+		new_G = np.delete(temp_G, elim, 1)
 
-		new_bits = "".join(x[i] for i in new_G.flatten())
-		# # print("test ", G[:i] + G[i+1:], "i is: ", i)
-		print(new_bits)
+		triu_inds = np.triu_indices(n-1, 1)
+		new_bits = "".join(x[i] for i in new_G[triu_inds])
+	
+		# first check ht
+		ht[new_bits] = calculate_prob(new_bits, new_G, n-1, ht)
 
-		# ht[G[:i] + G[i+1:]] = calculate_prob(G[:i] + G[i+1:], n-1, ht)
-		# test = 1/m * ht[G[:i] + G[i+1:]]
-
-		# need to somehow put it in the hash table
-		# print("????" , test, ht)
+		# update the probability array with these probabilities
+		prob[inds != elim] += 1/m * ht[new_bits]
+		
 		col += 1
-
 		if col > count:
 			row += 1
 			col = row + 1 
 			count -= 1
+
+	ht[bits] = prob
 	print("-------------------")
 	pass
 
@@ -97,7 +101,8 @@ if __name__ == "__main__":
 	
 	for bitgraph in y:
 		G = convert_binary_to_graph(bitgraph, 3)
-		calculate_prob(bitgraph, G, 3)
+		calculate_prob(bitgraph, G, 3, ht)
 		
 	# convert_binary_to_graph(y[1], 3)
-	print(ht)
+	for k, v in ht.items():
+		print(k, v)
